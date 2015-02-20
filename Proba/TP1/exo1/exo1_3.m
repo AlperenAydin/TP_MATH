f3173f8896fb4ca5993661bd024caf9f808805d0
AlperenAydin <aydin.alperen.bugra@gmail.com>
2015-02-20 17:34:28 +0100

    Proba TP1

---
+++ Proba/TP1/exo1/exo1_3.m
@@ -0,0 +1,18 @@
+clear all; close all; hold on;
+
+vals = 10:10:80; l = length(vals);
+theorie = zeros (1,l);
+empirique = zeros (1,l);
+
+for i = 1:l
+    valeur = vals(i);
+    theorie(i) = theorique(valeur);
+    empirique(i) = experience (valeur); 
+end
+
+
+plot (vals, [theorie;empirique]);
+legend ('- theorie','- empirique');
+grid on;
+xlabel('Nombre de gens');
+ylabel('Probabilite');
---
+++ Proba/TP1/exo2/test_noTruquee.m
@@ -0,0 +1,5 @@
+clear all; close all; hold on;
+
+x1 = LancerDeSixFaces(10^6);
+
+hist(x1,[1:1:6]);
